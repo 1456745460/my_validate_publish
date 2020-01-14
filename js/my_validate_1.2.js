c81d6034426f2ea9d727fa6caea445d2a2688186
王冰冰 <49932290+1456745460@users.noreply.github.com>
2020-01-14 10:50:24 +0800

    中英文长度
    增加中英文长度校验，汉字占两个字节

--- js/my_validate_1.2.js
+++ js/my_validate_1.2.js
@@ -1,24 +1,24 @@
-if (typeof (jQuery) == "undefined") {
-	console.log("%c表单校验插件基于jquery.js,请先引入..",'color:blue');
+if (typeof(jQuery) == "undefined") {
+	console.log("%c表单校验插件基于jquery.js,请先引入..", 'color:blue');
 }
-(function ($) { // 开启沙箱模式
-	$(function () {
+(function($) { // 开启沙箱模式
+	$(function() {
 
 		var CanSubmit = true; // 是否可以提交开关
-
-		$.fn.my_validate = function (obj) {
+		$.fn.my_validate = function(obj) {
 			var DeveloperMode = obj["DeveloperMode"] != undefined ? obj["DeveloperMode"] : false;
 			var subId = obj["subId"];
-			if(subId.indexOf(',')!=-1){
-				var arrD=subId.split(',');
-				var str='';
-				for(var nn=0;nn<arrD.length;nn++){
-					str+='#'+arrD[nn]+",";
+			// 处理 提交按钮多个的情况
+			if (subId.indexOf(',') != -1) {
+				var arrD = subId.split(',');
+				var str = '';
+				for (var nn = 0; nn < arrD.length; nn++) {
+					str += '#' + arrD[nn] + ",";
 				}
-				str=str.substring(0,str.length-1);
-				subId=str;
-			}else{
-				subId="#"+subId;
+				str = str.substring(0, str.length - 1);
+				subId = str;
+			} else {
+				subId = "#" + subId;
 			}
 			var formId = obj["formId"];
 			var ifAjax = obj["ifAjax"] != undefined ? obj["ifAjax"] : false;
@@ -27,11 +27,12 @@ if (typeof (jQuery) == "undefined") {
 			var ifImg = obj["ifImg"] != undefined ? obj["ifImg"] : [false, ""];
 			var submitSpecialData = obj["submitSpecialData"] != undefined ? obj["submitSpecialData"] : false;
 			var AutoScroll = obj["AutoScroll"] != undefined ? obj["AutoScroll"] : [false, 0];
-			var confirmSub = obj["confirmSub"] != undefined ? obj["confirmSub"] : [false, "",""];
+			var confirmSub = obj["confirmSub"]  != undefined ? obj["confirmSub"] : [false, "", ""];
+			var chineseLenth = obj["chineseLenth"] != undefined ? obj["chineseLenth"] : false;
 
 			if (DeveloperMode) {
-				console.log("%c已经启用开发者模式...",'color:blue');
-				console.log("%c正式环境请关闭开发者模式...",'color:blue');
+				console.log("%c已经启用开发者模式...", 'color:blue');
+				console.log("%c正式环境请关闭开发者模式...", 'color:blue');
 				if (document.getElementById(formId) == null) {
 					alert("formId配置错误!!未找到配置的form表单id!");
 					return false;
@@ -50,14 +51,14 @@ if (typeof (jQuery) == "undefined") {
 						return false;
 					}
 				} else if (ifLayer) {
-					if (typeof (layer) == "undefined") {
+					if (typeof(layer) == "undefined") {
 						alert("没有引入弹出层layer.js,请引入或关闭ifLayer:false");
 						return false;
 					}
 				}
 			}
 
-			var myJSON = checkJson();; // 获取配置JSON
+			var myJSON = checkJson(); // 获取配置JSON
 
 			var ScrollCount = 0; // 开启滚动，默认0
 
@@ -67,16 +68,16 @@ if (typeof (jQuery) == "undefined") {
 				for (var i = 0; i < myJSON.length; i++) {
 					$("#" + myJSON[i].id).addClass("vali_" + nowTime);
 				}
-				var realTimeStr="";
-				if(realTime[1].indexOf('-')>-1){
-					var realTimeArr=realTime[1].split('-');
-				    for(var nn=0;nn<realTimeArr.length;nn++){
-					   realTimeStr+=realTimeArr[nn]+" ";
-				    }
-				}else{
-					realTimeStr=realTime[1];
+				var realTimeStr = "";
+				if (realTime[1].indexOf('-') > -1) {
+					var realTimeArr = realTime[1].split('-');
+					for (var nn = 0; nn < realTimeArr.length; nn++) {
+						realTimeStr += realTimeArr[nn] + " ";
+					}
+				} else {
+					realTimeStr = realTime[1];
 				}
-				$(".myValidateForm_" + nowTime + " .vali_" + nowTime).on(realTimeStr, function () {
+				$(".myValidateForm_" + nowTime + " .vali_" + nowTime).on(realTimeStr, function() {
 					var t = $(this);
 					if (ifLayer) {
 						layer.closeAll();
@@ -120,16 +121,31 @@ if (typeof (jQuery) == "undefined") {
 
 					// 校验通过,如果没用layer 提示,置空校验错误提示
 					function removeHtml() {
-						if(!ifLayer){
+						if (!ifLayer) {
 							$("#" + myJSON[i].tid).html("");
 						}
 					}
+					
+					//  判断中英文长度
+					function strlen(str) {
+						var len = 0;
+						for (var i = 0; i < str.length; i++) {
+							var c = str.charCodeAt(i);
+							//单字节加1 
+							if ((c >= 0x0001 && c <= 0x007e) || (0xff60 <= c && c <= 0xff9f)) {
+								len++;
+							} else {
+								len += 2;
+							}
+						}
+						return len;
+					}
 
 					// 第一种,正则校验
-					if (typeof (myJSON[i].rules[j].reg) != 'undefined') {
+					if (typeof(myJSON[i].rules[j].reg) != 'undefined') {
 						// 再判断是否为空
 						if (myJSON[i].rules[j].reg.toString() == "/^.+$/") {
-							if (myJSON[i].rules[j].reg.test(val)|| val!="") {
+							if (myJSON[i].rules[j].reg.test(val) || val != "") {
 								removeHtml();
 							} else {
 								errorTips(myJSON[i].rules[j].tips);
@@ -146,17 +162,26 @@ if (typeof (jQuery) == "undefined") {
 
 					}
 					// 第二种,长度校验
-					else if (typeof (myJSON[i].rules[j].longest) != 'undefined') {
-						if (val.length > parseInt(myJSON[i].rules[j].longest) || val.length < parseInt(myJSON[i].rules[j].shortest)) {
-							errorTips(myJSON[i].rules[j].tips);
-							break;
-						} else {
-							removeHtml();
+					else if (typeof(myJSON[i].rules[j].longest) != 'undefined') {
+						 if(chineseLenth){
+							 if(strlen(val) > parseInt(myJSON[i].rules[j].longest) || strlen(val) < parseInt(myJSON[i].rules[j].shortest)){
+								 errorTips(myJSON[i].rules[j].tips);
+								 break;
+							 }else{
+								 removeHtml();
+							 }
+						 }else{
+							if (val.length > parseInt(myJSON[i].rules[j].longest) || val.length < parseInt(myJSON[i].rules[j].shortest)) {
+								errorTips(myJSON[i].rules[j].tips);
+								break;
+							} else {
+								removeHtml();
+							}
 						}
 					}
 					// 第三种,自定义校验
-					else if (typeof (myJSON[i].rules[j].custom) != 'undefined') {
-						if (!myJSON[i].rules[j].custom($("#"+myJSON[i].id))) {
+					else if (typeof(myJSON[i].rules[j].custom) != 'undefined') {
+						if (!myJSON[i].rules[j].custom($("#" + myJSON[i].id))) {
 							errorTips(myJSON[i].rules[j].tips);
 							break;
 						} else {
@@ -169,18 +194,21 @@ if (typeof (jQuery) == "undefined") {
 						if (DeveloperMode) {
 							alert("校验json配置错误!!!");
 						} else {
-							console.log("%c校验json配置错误!!!",'color:blue');
+							console.log("%c校验json配置错误!!!", 'color:blue');
 						}
 						break;
 					}
 				}
 			}
 
+
+            
+
 			// 点击提交按钮
-			$(subId).on('click', function () {
-				
-				var submitOrSave=$("#"+$(this).attr('id')).attr('submitOrSave');
-				
+			$(subId).on('click', function() {
+
+				var submitOrSave = $("#" + $(this).attr('id')).attr('submitOrSave');
+
 				CanSubmit = true;
 				if (ifLayer) {
 					layer.closeAll();
@@ -191,109 +219,110 @@ if (typeof (jQuery) == "undefined") {
 					var val = $("#" + getId).val();
 					eachVali(val, i);
 				}
-				
-				var confirmMsg='';
+
+				var confirmMsg = '';
 				//confirmSub
-				if(submitOrSave=='1'){
-					confirmMsg=confirmSub[1];
-				}else if(submitOrSave=='2'){
-					confirmMsg=confirmSub[2];
+				if (submitOrSave == '1') {
+					confirmMsg = confirmSub[1];
+				} else if (submitOrSave == '2') {
+					confirmMsg = confirmSub[2];
 				}
 
 				if (CanSubmit) {
 					// ajax 提交
 					if (ifAjax) {
-						if(confirmSub[0]){
+						if (confirmSub[0] && confirmMsg != '') {
 							layer.confirm(confirmMsg, {
-                        	btn: ['确定','取消'] //按钮
-                        	}, function(){
-                        		ajaxSub();
-                        	},function(){
-                        	});
-						}else{
+								btn: ['确定', '取消'] //按钮
+							}, function() {
+								ajaxSub();
+							}, function() {});
+						} else {
 							ajaxSub();
 						}
 					}
 					// 表单 提交
 					else if (ifAjax == false) {
-						if(confirmSub[0]){
+						if (confirmSub[0]) {
 							layer.confirm(confirmMsg, {
-                        	btn: ['确定','取消'] //按钮
-                        	}, function(){
-                        		$("#" + formId).submit();
-                        	},function(){
-                        	});
-						}else{
+								btn: ['确定', '取消'] //按钮
+							}, function() {
+								$("#" + formId).submit();
+							}, function() {});
+						} else {
 							$("#" + formId).submit();
 						}
 					}
 					// 跨域ajax 提交
 					else if (ifAjax == 'jsonp') {
-						if(confirmSub[0]){
+						if (confirmSub[0]) {
 							layer.confirm(confirmMsg, {
-                        	btn: ['确定','取消'] //按钮
-                        	}, function(){
-                        		ajaxJsoup();
-                        	},function(){
-                        	});
-						}else{
+								btn: ['确定', '取消'] //按钮
+							}, function() {
+								ajaxJsoup();
+							}, function() {});
+						} else {
 							ajaxJsoup();
 						}
 					}
-					
-					function ajaxSub(){
+
+					function ajaxSub() {
 						var mydata = '';
 						if (submitSpecialData) {
-							mydata = $.param(specialData()) + '&' + $("#" + formId).serialize()+'&'+$.param({submitOrSave:submitOrSave});
+							mydata = $.param(specialData()) + '&' + $("#" + formId).serialize() + '&' + $.param({
+								submitOrSave: submitOrSave
+							});
 						} else {
-							mydata = $("#" + formId).serialize()+'&'+$.param({submitOrSave:submitOrSave});
+							mydata = $("#" + formId).serialize() + '&' + $.param({
+								submitOrSave: submitOrSave
+							});
 						}
-						console.log("%c表单提交了如下参数：",'color:blue');
+						console.log("%c表单提交了如下参数：", 'color:blue');
 						console.log(specialData());
-						console.log("%c"+$("#" + formId).serialize(),'color:blue');
+						console.log("%c" + $("#" + formId).serialize(), 'color:blue');
 						$.ajax({
 							url: $("#" + formId).attr("action"),
 							type: $("#" + formId).attr("method"),
 							data: mydata,
 							dataType: "json",
 							async: false,
-							success: function (data) {
+							success: function(data) {
 								getAjax(data);
 							},
-							error: function (XMLHttpRequest, textStatus, errorThrown) {
+							error: function(XMLHttpRequest, textStatus, errorThrown) {
 								if (DeveloperMode) {
 									alert("ajax 提交表单报错了! 错误代码:" + XMLHttpRequest.status);
 								} else {
-									console.log("%cajax 提交表单报错了! 错误代码:" + XMLHttpRequest.status,'color:blue');
+									console.log("%cajax 提交表单报错了! 错误代码:" + XMLHttpRequest.status, 'color:blue');
 								}
 							}
 						});
 					}
-					
-					function ajaxJsoup(){
+
+					function ajaxJsoup() {
 						var mydata = '';
 						if (submitSpecialData) {
 							mydata = $.param(specialData()) + '&' + $("#" + formId).serialize();
 						} else {
 							mydata = $("#" + formId).serialize();
 						}
-						console.log("%c跨域表单提交了如下参数：",'color:blue');
+						console.log("%c跨域表单提交了如下参数：", 'color:blue');
 						console.log(specialData());
-						console.log("%c"+$("#" + formId).serialize(),'color:blue');
+						console.log("%c" + $("#" + formId).serialize(), 'color:blue');
 						$.ajax({
 							url: $("#" + formId).attr("action"),
 							type: "get",
 							data: mydata,
 							dataType: "jsonp",
 							jsonp: "jsoncallback",
-							success: function (data) {
+							success: function(data) {
 								getAjax(data);
 							},
-							error: function (XMLHttpRequest, textStatus, errorThrown) {
+							error: function(XMLHttpRequest, textStatus, errorThrown) {
 								if (DeveloperMode) {
 									alert("ajax 跨域 提交表单报错了! 错误代码:" + XMLHttpRequest.status);
 								} else {
-									console.log("%cajax 跨域 提交表单报错了! 错误代码:" + XMLHttpRequest.status,'color:blue');
+									console.log("%cajax 跨域 提交表单报错了! 错误代码:" + XMLHttpRequest.status, 'color:blue');
 								}
 							}
 						});
@@ -307,35 +336,35 @@ if (typeof (jQuery) == "undefined") {
 	// 快捷调用常用正则
 	zz = { // 定义一个 名为 zz 的方法,在这里添加常用正则, 例如 zz.notNull()  调用
 
-		notNull: function () {
+		notNull: function() {
 			return /^.+$/;
 		},
 		// 不能为空
-		chinese: function () {
+		chinese: function() {
 			return /^[\u4e00-\u9fa5]+$/;
 		}, // 中文
-		email: function () {
+		email: function() {
 			return /^([a-zA-Z0-9_-])+@([a-zA-Z0-9_-])+((\.[a-zA-Z0-9_-]{2,3}){1,2})$/;
 		}, // 邮箱
-		post: function () {
+		post: function() {
 			return /^[1-9][0-9]{0,}$/;
 		}, //邮政编码
-		num: function () {
+		num: function() {
 			return /^[0-9]*$/;
 		}, // 数字
-		ffzs: function () {
+		ffzs: function() {
 			return /^\d+$/;
 		}, // 非负整数
-		aBc123: function () {
+		aBc123: function() {
 			return /^[A-Za-z0-9]+$/;
 		}, //数字和英文
-		mobile: function () {
+		mobile: function() {
 			return /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/;
 		}, //手机号
-		tel: function () {
+		tel: function() {
 			return /^1[0-9]{10}$/;
 		}, // 手机号
-		fax: function () {
+		fax: function() {
 			return /^((\d{7,8})|(\d{4}|\d{3})-(\d{7,8})|(\d{4}|\d{3})-(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1})|(\d{7,8})-(\d{4}|\d{3}|\d{2}|\d{1}))$/;
 		} //传真
 
